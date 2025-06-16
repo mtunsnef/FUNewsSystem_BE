ee422866ff6d4a742eaafa8e4ba5e0b6c0f84bb2
mtunsnef <tuannm23.dev@gmail.com>
2025-06-17 00:35:37 +0700

    perf: repository base

---
+++ FUNewsSystem.Infrastructure/Repositories/IRepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsSystem.Infrastructure.Repositories
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        Task<List<TEntity>> GetAllAsync();
+        Task<TEntity?> GetByIdAsync(object id);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+    }
+}
---
+++ FUNewsSystem.Infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,47 @@
+﻿using FUNewsSystem.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsSystem.Infrastructure.Repositories
+{
+    public class RepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : class
+    {
+        protected readonly FunewsSystemApiDbContext _context;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public RepositoryBase(FunewsSystemApiDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public virtual async Task<List<TEntity>> GetAllAsync()
+            => await _dbSet.AsQueryable().ToListAsync();
+
+        public virtual async Task<TEntity?> GetByIdAsync(object id)
+            => await _dbSet.FindAsync(id);
+
+        public virtual async Task AddAsync(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task UpdateAsync(TEntity entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteAsync(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
