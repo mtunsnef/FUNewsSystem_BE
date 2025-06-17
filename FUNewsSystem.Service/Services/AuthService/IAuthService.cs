4c19bb9e3034511d3c27fba9deee32b68409c3b0
mtunsnef <tuannm23.dev@gmail.com>
2025-06-18 02:04:08 +0700

    feat: login an account

---
+++ FUNewsSystem.Service/Services/AuthService/IAuthService.cs
@@ -0,0 +1,23 @@
+﻿using FUNewsSystem.Domain.Models;
+using FUNewsSystem.Service.DTOs.AuthDto;
+using FUNewsSystem.Service.DTOs.AuthDto.ExternalDto;
+using FUNewsSystem.Service.DTOs.ResponseDto;
+using FUNewsSystem.Service.DTOs.SystemAccountDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsSystem.Service.Services.AuthService
+{
+    public interface IAuthService
+    {
+        Task<SystemAccount> GetMe();
+        Task LogoutAsync(LogoutRequestDto request);
+        Task<TokenPayloadDto> RefreshTokenAsync(string token);
+        Task<LoginPayloadDto> Login(UserCredentialDto dto);
+        Task<ExternalLoginPayloadDto> HandleExternalLoginAsync(string email, string? name, string externalId);
+        Task<ApiResponseDto<string>> CompleteRegisterAsync(CompleteExternalRegisterDto dto);
+    }
+}
