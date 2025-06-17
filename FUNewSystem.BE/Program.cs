290209a4ba03db5eccdd7467b52cd2a13b3040a7
mtunsnef <tuannm23.dev@gmail.com>
2025-06-18 01:47:32 +0700

    refactors: add middleware for authorization result handles

---
+++ FUNewSystem.BE/Program.cs
@@ -0,0 +1,231 @@
+﻿
+using FluentValidation.AspNetCore;
+using FUNewsSystem.Domain.Consts;
+using FUNewsSystem.Domain.Models;
+using FUNewsSystem.Infrastructure.Repositories.CategoryRepo;
+using FUNewsSystem.Infrastructure.Repositories.InvalidatedTokenRepo;
+using FUNewsSystem.Infrastructure.Repositories.NewsArticleRepo;
+using FUNewsSystem.Infrastructure.Repositories.SystemAccountRepo;
+using FUNewsSystem.Infrastructure.Repositories.TagRepo;
+using FUNewsSystem.Service.AutoMapper;
+using FUNewsSystem.Service.Services.AuthService;
+using FUNewsSystem.Service.Services.CategoryService;
+using FUNewsSystem.Service.Services.ConfigService;
+using FUNewsSystem.Service.Services.HttpContextService;
+using FUNewsSystem.Service.Services.NewsArticleService;
+using FUNewsSystem.Service.Services.SystemAccountService;
+using FUNewsSystem.Service.Services.TagService;
+using FUNewSystem.BE.Middlewares;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.OData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace FUNewSystem.BE
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers()
+                .AddOData(options =>
+                    options.Select()
+                           .Filter()
+                           .Count()
+                           .OrderBy()
+                           .Expand()
+                           .SetMaxTop(100))
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                });
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "News Swagger API",
+                    Description = "An ASP.NET Core Web API for FU News System App",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    License = new OpenApiLicense
+                    {
+                        Name = "Example License",
+                        Url = new Uri("https://example.com/license")
+                    }
+                });
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme.
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+
+
+            //Add cors
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowFrontend", policy =>
+                {
+                    policy.WithOrigins(
+                                "https://localhost:44352",
+                                "https://localhost:7157",
+                                "https://funews.azurewebsites.net"
+                            )
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials();
+                });
+            });
+
+            // Add Database configuration
+            builder.Services.AddDbContext<FunewsSystemApiDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("FUNewsConnection"));
+            });
+
+            var secret = builder.Configuration["Jwt:SecretKey"]!.Trim();
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+            //Add Authenticaion and Authorization
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = AuthSchemes.Cookie;
+            })
+                .AddCookie(AuthSchemes.Cookie)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = key,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        ClockSkew = TimeSpan.Zero
+                    };
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnTokenValidated = async context =>
+                        {
+                            var jti = context.Principal?.FindFirst(JwtRegisteredClaimNames.Jti)?.Value;
+                            var blacklistService = context.HttpContext.RequestServices.GetRequiredService<IBlacklistTokenService>();
+                            if (!string.IsNullOrEmpty(jti))
+                            {
+                                var isBlacklisted = await blacklistService.IsBlacklistedAsync(jti);
+                                if (isBlacklisted)
+                                {
+                                    context.Fail("Token is blacklisted.");
+                                }
+                            }
+                        }
+                    };
+                })
+                .AddGoogle(AuthSchemes.Google, options =>
+                {
+                    options.ClientId = builder.Configuration["AuthSettings:Google:ClientId"];
+                    options.ClientSecret = builder.Configuration["AuthSettings:Google:ClientSecret"];
+                    options.CallbackPath = "/Auth/signIn-google";
+                })
+                .AddFacebook(AuthSchemes.Facebook, options =>
+                {
+                    options.AppId = builder.Configuration["AuthSettings:Facebook:AppId"];
+                    options.AppSecret = builder.Configuration["AuthSettings:Facebook:AppSecret"];
+                    options.CallbackPath = "/Auth/signIn-facebook";
+                    options.Events = new OAuthEvents
+                    {
+                        OnRemoteFailure = context =>
+                        {
+                            context.Response.Redirect("/dang-nhap");
+                            context.HandleResponse();
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+
+            builder.Services.AddAuthorization();
+            builder.Services.AddSingleton<IAuthorizationMiddlewareResultHandler, CustomAuthorizationMiddlewareResultHandler>();
+
+            // Add Http Context Accessor
+            builder.Services.AddHttpContextAccessor();
+
+            // Add Fluent Validation
+            builder.Services.AddFluentValidationAutoValidation();
+
+            //Repository
+            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+            builder.Services.AddScoped<INewsArticleRepository, NewsArticleRepository>();
+            builder.Services.AddScoped<ITagRepository, TagRepository>();
+            builder.Services.AddScoped<ISystemAccountRepository, SystemAccountRepository>();
+            builder.Services.AddScoped<IBlacklistTokenRepository, BlacklistTokenRepository>();
+
+            //Service
+            builder.Services.AddScoped<ICategoryService, CategoryService>();
+            builder.Services.AddScoped<INewsArticleService, NewsArticleService>();
+            builder.Services.AddScoped<ITagService, TagService>();
+            builder.Services.AddScoped<ISystemAccountService, SystemAccountService>();
+            builder.Services.AddScoped<IAuthService, AuthService>();
+            builder.Services.AddScoped<IConfigService, ConfigService>();
+            builder.Services.AddScoped<IHttpContextService, HttpContextService>();
+            builder.Services.AddScoped<IBlacklistTokenService, BlacklistTokenService>();
+
+            //Config automapper
+            builder.Services.AddAutoMapper(typeof(CategoryProfile));
+            builder.Services.AddAutoMapper(typeof(Program));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseGlobalExceptionHandler();
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseCors("AllowFrontend");
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ FUNewsSystem.Domain/Enums/SystemAccount/UserRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsSystem.Domain.Enums.SystemAccount
+{
+    public enum UserRole
+    {
+        Staff = 1,
+        Lecturer = 2,
+        Admin = 3,
+    }
+}
