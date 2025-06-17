534ab47a11abbcc86d3bb6092aeb05c3c41f3579
mtunsnef <tuannm23.dev@gmail.com>
2025-06-18 01:43:05 +0700

    feat: add endpoint api/auth/token

---
+++ FUNewsSystem.Domain/Enums/Auth/AuthToken.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsSystem.Domain.Enums.Auth
+{
+    public enum AuthToken
+    {
+        AccessToken,
+        RefreshToken
+    }
+}
---
+++ FUNewsSystem.Infrastructure/Repositories/SystemAccountRepo/SystemAccountRepository.cs
@@ -0,0 +1,24 @@
+﻿using FUNewsSystem.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsSystem.Infrastructure.Repositories.SystemAccountRepo
+{
+    public class SystemAccountRepository : RepositoryBase<SystemAccount>, ISystemAccountRepository
+    {
+        public SystemAccountRepository(FunewsSystemApiDbContext context) : base(context)
+        {
+        }
+
+        public async Task<SystemAccount?> GetAccountByEmail(string email)
+        {
+            var appUser = await _dbSet.FirstOrDefaultAsync(u => u.AccountEmail == email);
+
+            return appUser;
+        }
+    }
+}
