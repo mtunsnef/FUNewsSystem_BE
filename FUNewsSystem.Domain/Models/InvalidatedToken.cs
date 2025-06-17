927423764c87506af2301997040707d3399f8231
mtunsnef <tuannm23.dev@gmail.com>
2025-06-18 01:41:27 +0700

    docs: add models

---
+++ FUNewsSystem.Domain/Models/InvalidatedToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FUNewsSystem.Domain.Models;
+
+public partial class InvalidatedToken
+{
+    public int Id { get; set; }
+
+    public string Token { get; set; } = null!;
+
+    public DateTime ExpiryTime { get; set; }
+}
---
+++ FUNewsSystem.Domain/Models/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FUNewsSystem.Domain.Models;
+
+public partial class Tag
+{
+    public string TagId { get; set; } = null!;
+
+    public string? TagName { get; set; }
+
+    public string? Note { get; set; }
+
+    public virtual ICollection<NewsArticle> NewsArticles { get; set; } = new List<NewsArticle>();
+}
