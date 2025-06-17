2bd47e202d7ba53396c4754bdf4bb4bfd5df6e80
mtunsnef <tuannm23.dev@gmail.com>
2025-06-18 02:02:18 +0700

    docs: add models

---
+++ FUNewsSystem.Domain/Models/SystemAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FUNewsSystem.Domain.Models;
+
+public partial class SystemAccount
+{
+    public string AccountId { get; set; } = null!;
+
+    public string? AccountName { get; set; }
+
+    public string? AccountEmail { get; set; }
+
+    public int? AccountRole { get; set; }
+
+    public string? AccountPassword { get; set; }
+
+    public string? AuthProvider { get; set; }
+
+    public string? AuthProviderId { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public virtual ICollection<NewsArticle> NewsArticles { get; set; } = new List<NewsArticle>();
+
+    public virtual ICollection<Notification> Notifications { get; set; } = new List<Notification>();
+}
