ffc9c8536139b43470e68bf061268afa59150600
Nguyen Minh Tuan <tuannm23.dev@gmail.com>
2025-06-18 01:52:34 +0700

    fix: update context

--- FUNewsSystem.Domain/Models/FunewsSystemApiDbContext.cs
+++ FUNewsSystem.Domain/Models/FunewsSystemApiDbContext.cs
@@ -28,8 +28,16 @@ public partial class FunewsSystemApiDbContext : DbContext
     public virtual DbSet<Tag> Tags { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:funewssystemapidbserver.database.windows.net,1433;Initial Catalog=FUNewsSystem.API_db;Persist Security Info=False;User ID=minhtun;Password=Endraw2311@K17;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+    {
+        optionsBuilder.UseSqlServer(GetConnectionString());
+    }
+    private string GetConnectionString()
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true).Build();
+        return configuration["ConnectionStrings:FUNewsConnection"];
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
