6187c0378d364495ba3b3f70ddea90fdbb6d6837
mtunsnef <tuannm23.dev@gmail.com>
2025-06-19 00:43:41 +0700

    feat: crud system account

---
+++ FUNewsSystem.Service/Services/SystemAccountService/ISystemAccountService.cs
@@ -0,0 +1,23 @@
+﻿using FUNewsSystem.Domain.Models;
+using FUNewsSystem.Service.DTOs.AuthDto.ExternalDto;
+using FUNewsSystem.Service.DTOs.ResponseDto;
+using FUNewsSystem.Service.DTOs.SystemAccountDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsSystem.Service.Services.SystemAccountService
+{
+    public interface ISystemAccountService
+    {
+        Task<SystemAccount?> GetUserByIdAsync(string id);
+        Task<List<SystemAccount>> GetAllAsync();
+        Task<ApiResponseDto<string>> CreateAccount(CreateUpdateSystemAccountDto dto);
+        Task<ApiResponseDto<string>> UpdateAccount(string id, CreateUpdateSystemAccountDto dto);
+        Task<ApiResponseDto<string>> DeleteAccount(string id);
+        Task<SystemAccount?> GetAccountByEmail(string email);
+        Task<SystemAccount> CreateExternalAccountAsync(CreateSystemAccountDto dto);
+    }
+}
