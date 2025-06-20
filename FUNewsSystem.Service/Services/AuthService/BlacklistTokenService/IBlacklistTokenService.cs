02c2ebca589cd2964bf6a6475b1c47b899314638
mtunsnef <tuannm23.dev@gmail.com>
2025-06-20 21:26:01 +0700

    fix: fix blacklistToken storage to redis

---
+++ FUNewsSystem.Service/Services/AuthService/BlacklistTokenService/IBlacklistTokenService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsSystem.Service.Services.AuthService.BlacklistTokenService
+{
+    public interface IBlacklistTokenService
+    {
+        Task<bool> IsBlacklistedAsync(string jti);
+        Task BlacklistAsync(string jti, DateTime expiresAt);
+    }
+}
