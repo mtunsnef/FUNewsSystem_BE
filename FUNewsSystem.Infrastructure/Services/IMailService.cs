348c3fef2117ce52be2920a04ddcba6ee99c24f5
mtunsnef <tuannm23.dev@gmail.com>
2025-07-03 16:35:17 +0700

    feat: send email

---
+++ FUNewsSystem.Infrastructure/Services/IMailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsSystem.Infrastructure.Services
+{
+    public interface IMailService
+    {
+        Task<bool> SendMail(MailContent mailContent);
+    }
+}
